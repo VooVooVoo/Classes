5bede7153d5cf6ccc6525bcb7b69381ec99db2c9
Wojt <ciechman.ws@gmail.com>
2016-08-01 12:32:14 +0200

    All members, object

--- src/main/java/GroupMember.java
+++ src/main/java/GroupMember.java
@@ -1,13 +1,13 @@
 /**
  * Created by Wojt on 2016-07-31.
  */
-public class Groupmember {
+public class GroupMember {
 
     private String name;
     private long codepower;
     private long codespeed;
 
-    public Groupmember(String name, long codepower, long codespeed) {
+    public GroupMember(String name, long codepower, long codespeed) {
 
         this.name = name;
         this.codepower = codepower;
@@ -17,9 +17,9 @@ public class Groupmember {
     public String toString() {
         return "Member: " + name + ", The Power of Code " + codepower + ", Coding speed: " + codespeed;
     }
-    public void supportMember(Groupmember groupmembera, Groupmember groupmemberb) {
-        groupmemberb.codepower += 0.01 * groupmembera.codepower;
-        groupmemberb.codespeed += 0.01 * groupmembera.codespeed;
+    public void supportMember(GroupMember groupmemberb) {
+        groupmemberb.codepower += 0.01 * codepower;
+        groupmemberb.codespeed += 0.01 * codespeed;
     }
 
 }
