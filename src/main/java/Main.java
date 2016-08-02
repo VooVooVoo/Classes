281944a2a3a40a976bb4e3e14e78c3adf6e6a9d4
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-08-02 14:13:18 +0200

    Refactoring: extracetd  2 methods;  Trainer gains skill as well

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[]args) {
 
         ArrayList<GroupMember> groupList = new ArrayList<>();
-
         groupList.add(new GroupMember("Anawoj", 100, 100 ));
         groupList.add(new GroupMember("Karo", 100, 100));
         groupList.add(new GroupMember("Merrylouiss",100, 100));
@@ -16,132 +15,94 @@ public class Main {
         groupList.add(new GroupMember("Voyt", 100, 100));
         groupList.add(new GroupMember("Piotr", 9001, 3000));
 
-        int i = 0;
-        while (i <groupList.size()) {
-            GroupMember groupMember = groupList.get(i);
-            System.out.println(groupList.get(i));
-            i += 1;
-        }
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Day one training. WOWOWOW!!! Peter's Power is over 9000!");
-        new Scanner(System.in).nextLine();
-        System.out.println("I, for one, welcome our CodeLord.");
-        new Scanner(System.in).nextLine();
-        System.out.println("Shall we start, then?");
-        new Scanner(System.in).nextLine();
+
+// Shorter version
+//        writeListToConsole(groupList);
+//        for(int dayNumber = 1; dayNumber <= 5; dayNumber++){
+//            writeAndWait(scanner, "Day number: " + dayNumber+ ". Shall we start, then?");
+//            powerUpGroup(groupList);
+//            writeListToConsole(groupList);
+//        }
+
+
+
+
+        writeListToConsole(groupList);
+
+        writeAndWait(scanner, "Day one training. WOWOWOW!!! Peter's Power is over 9000!");
+        writeAndWait(scanner, "I, for one, welcome our CodeLord.");
+        writeAndWait(scanner, "Shall we start, then?");
 
         System.out.println("Peter uses his teaching skills...I can see the code!");
 
-        groupList.get(6).supportMember(groupList.get(0));
-        groupList.get(6).supportMember(groupList.get(1));
-        groupList.get(6).supportMember(groupList.get(2));
-        groupList.get(6).supportMember(groupList.get(3));
-        groupList.get(6).supportMember(groupList.get(4));
-        groupList.get(6).supportMember(groupList.get(5));
+        powerUpGroup(groupList);
+
+//        for (GroupMember supportingGroupMember :groupList){
+//            for(GroupMember supportedGroupMember :groupList) {
+//                supportingGroupMember.supportMember(supportedGroupMember);
+//            }
+//        }
 
 
-        i = 0;
-        while (i <groupList.size()) {
-            GroupMember groupMember = groupList.get(i);
-            System.out.println(groupList.get(i));
-            i += 1;
-        }
-
-
-        System.out.println("Day two training. Do you want to commence?");
-        new Scanner(System.in).nextLine();
+        writeListToConsole(groupList);
+        writeAndWait(scanner, "Day two training. Do you want to commence?");
 
         System.out.println("Peter uses his teaching skills...OMG!!!");
 
-        groupList.get(6).supportMember(groupList.get(0));
-        groupList.get(6).supportMember(groupList.get(1));
-        groupList.get(6).supportMember(groupList.get(2));
-        groupList.get(6).supportMember(groupList.get(3));
-        groupList.get(6).supportMember(groupList.get(4));
-        groupList.get(6).supportMember(groupList.get(5));
+        powerUpGroup(groupList);
 
-        i = 0;
-        while (i <groupList.size()) {
-            GroupMember groupMember = groupList.get(i);
-            System.out.println(groupList.get(i));
-            i += 1;
-        }
-
-        System.out.println("Day three training. Do you want to commence?");
-        new Scanner(System.in).nextLine();
+        writeListToConsole(groupList);
+        writeAndWait(scanner, "Day three training. Do you want to commence?");
 
         System.out.println("Peter uses his teaching skills...See how much we have learnt?!?!");
 
-        groupList.get(6).supportMember(groupList.get(0));
-        groupList.get(6).supportMember(groupList.get(1));
-        groupList.get(6).supportMember(groupList.get(2));
-        groupList.get(6).supportMember(groupList.get(3));
-        groupList.get(6).supportMember(groupList.get(4));
-        groupList.get(6).supportMember(groupList.get(5));
+        powerUpGroup(groupList);
+        writeListToConsole(groupList);
 
-        i = 0;
-        while (i <groupList.size()) {
-            GroupMember groupMember = groupList.get(i);
-            System.out.println(groupList.get(i));
-            i += 1;
-        }
-
-        System.out.println("Day four training. Do you want to commence?");
-        new Scanner(System.in).nextLine();
+        writeAndWait(scanner, "Day four training. Do you want to commence?");
 
         System.out.println("Peter uses his teaching skills...Just think of all the possible applications!!!");
 
-        groupList.get(6).supportMember(groupList.get(0));
-        groupList.get(6).supportMember(groupList.get(1));
-        groupList.get(6).supportMember(groupList.get(2));
-        groupList.get(6).supportMember(groupList.get(3));
-        groupList.get(6).supportMember(groupList.get(4));
-        groupList.get(6).supportMember(groupList.get(5));
+        powerUpGroup(groupList);
+        writeListToConsole(groupList);
 
-        i = 0;
-        while (i <groupList.size()) {
-            GroupMember groupMember = groupList.get(i);
-            System.out.println(groupList.get(i));
-            i += 1;
-        }
-
-        System.out.println("Day five training. Do you want to commence?");
-        new Scanner(System.in).nextLine();
+        writeAndWait(scanner, "Day five training. Do you want to commence?");
 
         System.out.println("Peter uses his teaching skills...That's awesome!");
 
-        groupList.get(6).supportMember(groupList.get(0));
-        groupList.get(6).supportMember(groupList.get(1));
-        groupList.get(6).supportMember(groupList.get(2));
-        groupList.get(6).supportMember(groupList.get(3));
-        groupList.get(6).supportMember(groupList.get(4));
-        groupList.get(6).supportMember(groupList.get(5));
+        powerUpGroup(groupList);
+        writeListToConsole(groupList);
 
-        i = 0;
+        writeAndWait(scanner, "It's weekend. Go home.");
+        writeAndWait(scanner, "No, really. Have some rest.");
+        writeAndWait(scanner, "Seriously, mate. Go home and chill.");
+        writeAndWait(scanner, "Ok, I'll see myself out! ");
+        writeAndWait(scanner, "Bye!");
+        writeAndWait(scanner, "I'm not good at saying goodbyes...");
+        writeAndWait(scanner, "This time it's serious, though.");
+        System.out.println("I've taken the red pill.");
+
+    }
+
+    private static void writeAndWait(Scanner scanner, String x) {
+        System.out.println(x);
+        scanner.nextLine();
+    }
+
+    private static void powerUpGroup(ArrayList<GroupMember> groupList) {
+        for (GroupMember groupMember :groupList){
+            groupList.get(6).supportMember(groupMember);
+        }
+    }
+
+    private static void writeListToConsole(ArrayList<GroupMember> groupList) {
+        int i = 0;
         while (i <groupList.size()) {
             GroupMember groupMember = groupList.get(i);
-            System.out.println(groupList.get(i));
+            System.out.println(groupMember);
             i += 1;
         }
-
-
-        System.out.println("It's weekend. Go home.");
-        new Scanner(System.in).nextLine();
-        System.out.println("No, really. Have some rest.");
-        new Scanner(System.in).nextLine();
-        System.out.println("Seriously, mate. Go home and chill.");
-        new Scanner(System.in).nextLine();
-        System.out.println("Ok, I'll see myself out! ");
-        new Scanner(System.in).nextLine();
-        System.out.println("Bye!");
-        new Scanner(System.in).nextLine();
-        System.out.println("I'm not good at saying goodbyes...");
-        new Scanner(System.in).nextLine();
-        System.out.println("This time it's serious, though.");
-        new Scanner(System.in).nextLine();
-        System.out.println("I've taken the red pill.");
-        
-
-
     }
 }
